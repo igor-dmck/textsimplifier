87005bb7401e8064a492730d364ce960a5116ce2
igor-dmck <igordmck@gmail.com>
2018-06-26 00:06:25 +0300

    replace_duplicates_fix_and_add_methods

--- src/main/java/com/itrexgroup/adamchik/TextSimplifier.java
+++ src/main/java/com/itrexgroup/adamchik/TextSimplifier.java
@@ -1,49 +1,75 @@
 package com.itrexgroup.adamchik;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 
 public class TextSimplifier {
     public static void main(String[] args) {
 
-        String input = "Coffee, The table, doodeee!";
+        String input = "Success, Coffee, cimple, an apple, The Ttable, Lucky, Ttuuuuuuuute, a doodeee!";
 
-        String str = simpleText(input);
-
-
-        System.out.println(str);
+        removeArt3(removeArt2(removeE(removeArt1(replaceDuplicates(replaceO(replaceE(replaceC(replaceCk(replaceCi(replaceCe(input)))))))))));
 
     }
 
-    public static String simpleText(String input) {
+    public static String replaceCe(String input) {
 
-        String removeCe = input.replaceAll("(?i)(ce)","se");
-        System.out.println(removeCe);
+        String resultText = input.replaceAll("(?i)(ce)", "se");
+        System.out.println(resultText);
+        return resultText;
+    }
 
-        String replaceCk = removeCe.replaceAll("(?i)c","k");
-        System.out.println(replaceCk);
+    public static String replaceCi(String input) {
+        String resultText = input.replaceAll("(?i)ci", "si");
+        System.out.println(resultText);
+        return resultText;
+    }
 
-        String replaceC = replaceCk.replaceAll("(?i)c","k");
-        System.out.println(replaceC);
+    public static String replaceCk(String input) {
+        String resultText = input.replaceAll("(?i)ck", "k");
+        System.out.println(resultText);
+        return resultText;
+    }
 
-        String replE = replaceC.replaceAll("e{2}", "i");
-        System.out.println(replE);
-
-        String replO = replE.replaceAll("o{2}", "u");
-        System.out.println(replO);
-
-        String replaceDuplicates = replO.replaceAll("(\\w)\\1","$1");
-        System.out.println(replaceDuplicates);
-
-        String remArt1 = replaceDuplicates.replaceAll("(?<![a-zA-Z])(?i)the(?![a-zA-Z])","");
-
-        String removeLastE = remArt1.replaceAll("(?<!\\b)e\\b","");
-        System.out.println(removeLastE);
-
-        String remArt2 = removeLastE.replaceAll("(?<![a-zA-Z])(?i)an(?![a-zA-Z])","");
-        return remArt2.replaceAll("(?<![a-zA-Z])(?i)a(?![a-zA-Z])","");
+    public static String replaceC(String input) {
+        String resultText = input.replaceAll("(?i)c", "k");
+        System.out.println(resultText);
+        return resultText;
+    }
 
 
+    public static String replaceE(String input) {
+        String resultText = input.replaceAll("e{2}", "i");
+        System.out.println(resultText);
+        return resultText;
+    }
+
+    public static String replaceO(String input) {
+        String resultText = input.replaceAll("o{2}", "u");
+        System.out.println(resultText);
+        return resultText;
+    }
+    public static String replaceDuplicates(String input) {
+        String resultText = input.replaceAll("(\\w)\\1+","$1");
+        System.out.println(resultText);
+        return resultText;
+    }
+    public static String removeArt1(String input) {
+        String resultText = input.replaceAll("(?<![a-zA-Z])(?i)the(?![a-zA-Z])","");
+        System.out.println(resultText);
+        return resultText;
+    }
+    public static String removeE(String input) {
+        String resultText = input.replaceAll("(?<!\\b)e\\b","");
+        System.out.println(resultText);
+        return resultText;
+    }
+    public static String removeArt2(String input) {
+        String resultText = input.replaceAll("(?<![a-zA-Z])(?i)an(?![a-zA-Z])","");
+        System.out.println(resultText);
+        return resultText;
+    }
+    public static String removeArt3(String input) {
+        String resultText = input.replaceAll("(?<![a-zA-Z])(?i)a(?![a-zA-Z])", "");
+        System.out.println(resultText);
+        return resultText;
     }
 }
